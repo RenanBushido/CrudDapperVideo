36478c40896249cf3753d276d150cf7fc4a9c6a0
Renan M. Cechelero <renan79@gmail.com>
2024-08-27 01:27:37 -0300

    Finalizacao do Metodo Get

---
+++ CrudDapperVideo/Services/IUsuarioInterfaces.cs
@@ -0,0 +1,10 @@
+﻿using CrudDapperVideo.Dto;
+using CrudDapperVideo.Models;
+
+namespace CrudDapperVideo.Services
+{
+    public interface IUsuarioInterfaces
+    {
+        Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios();
+    }
+}
